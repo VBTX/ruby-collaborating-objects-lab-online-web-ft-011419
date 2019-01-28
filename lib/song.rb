d61b19511a2ce3b1f79bc4df97b93711cff4756b
Victoria B <viktorija.bourassa@gmail.com>
2019-01-28 22:51:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(file_name)
     name = file_name.split(" - ")[1]
     song = self.new(name)
-    song
+    song.artist = file_name.split(" - ")[0]
   end
 
 end

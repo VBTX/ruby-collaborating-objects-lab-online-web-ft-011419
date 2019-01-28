7ab53bfc6fba98d2cab1dc670e2fe49b9f92ddb4
Victoria B <viktorija.bourassa@gmail.com>
2019-01-28 20:53:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,8 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(file)
-    file = Song.new_by_filename
+  def self.new_by_filename(file_name)
+    file = Song.new_by_filename(file_name)
     song = file.artist
   end
 

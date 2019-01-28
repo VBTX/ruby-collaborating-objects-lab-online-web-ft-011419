25e9417bbbb40931959930f2c8f86fef8f71b181
Victoria B <viktorija.bourassa@gmail.com>
2019-01-28 23:22:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,9 +8,6 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs
-  end
 
   def add_song(song)
     @songs << song
@@ -42,6 +39,9 @@ class Artist
     @songs.each do |song|
       puts song.name
     end
+  end
+  def songs
+    @songs
   end
 
 end

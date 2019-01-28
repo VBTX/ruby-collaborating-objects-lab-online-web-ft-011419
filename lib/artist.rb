f190009b03f4153a6fce3bc7267c08ec6e242384
Victoria B <viktorija.bourassa@gmail.com>
2019-01-28 22:03:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     if (self.name.nil?)
-      self.name = Artist.new(name)
+      self = Artist.new(name)
     else
       Artist.name
     end
